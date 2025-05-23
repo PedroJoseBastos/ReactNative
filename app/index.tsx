9ee06ae1ce46613a0ea8f0b93ffb692c1c08c6ce
PedroJoseBastos <pjbb.bastos@gmail.com>
2025-05-23 19:33:07 -0400

    feitoex1

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { Button, Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+      
+      <Image
+        className="w-32 h-32 rounded-full" 
+        source={{ uri: "https://upload.wikimedia.org/wikipedia/commons/a/a1/Zeca_Pagodinho.jpg" }}
+      /> 
+  
+      <Text className="text-3xl text-dark-100 font-bold">Zeca Pagodinho</Text>
+      <Text className="text-lg">Idade: 30 anos</Text>
+      <Text className="text-lg">Localização: Rio de Janeiro</Text>
+      
+      <View className="mt-4">
+        <Button title="Editar Perfil" onPress={() => {}} />
+      </View>
+
     </View>
   );
 }
